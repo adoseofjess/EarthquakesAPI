aef8bdd201e6620c311f0c921839b7cd6c4cb835
Jessica J. Kim <jessjkim@gmail.com>
2014-01-24 16:21:36 -0800

    Write JSON API controller actions.

--- app/controllers/earthquakes_controller.rb
+++ app/controllers/earthquakes_controller.rb
@@ -4,16 +4,16 @@ class EarthquakesController < ApplicationController
   def index
     if params[:on].nil? && params[:since].nil? && params[:over].nil? && params[:near].nil?
       @earthquakes = Earthquake.all
+      p @earthquakes.length
       render :json => @earthquakes
     else 
-      # p params[:near].split(",")
       @earthquakes = Earthquake.all
       if !params[:on].nil?
-        date = Time.at(params[:on])
+        date = Time.at(params[:on].to_i)
         @earthquakes = Earthquake.find_by_date(date)
       end
       if !params[:since].nil?
-        date_since = Time.at(params[:since])
+        date_since = Time.at(params[:since].to_i)
         @earthquakes = Earthquake.where("date > :date_since", date_since: date_since)
       end
       if !params[:over].nil?
@@ -21,8 +21,16 @@ class EarthquakesController < ApplicationController
         @earthquakes = Earthquake.where("magnitude > :magnitude_given", magnitude_given: magnitude_given)
       end
       if !params[:near].nil?
-        lat_lng = params[:near]
+        lat_lng1 = []
+        params[:near].split(",").each do |coord|
+          lat_lng1.push(coord.to_f)
+        end
+        @earthquakes.select! {|earthquake| 
+        lat_lng2 = []       
+lat_lng2.push(earthquake.latitude.to_f).push(earthquake.longitude.to_f)
         
+        Geocoder::Calculations.distance_between(lat_lng1, lat_lng2) < 5.0
+        }
       end
       render :json => @earthquakes
     end
